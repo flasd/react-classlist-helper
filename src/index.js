429b2b2b6cb7754bc60808f6a57d0393455fb2ff
flasd <flasd2@gmail.com>
2017-07-08 23:20:50 -0300

    Added toggleClass function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import isObject from 'lodash.isobject';
 import isString from 'lodash.isstring';
 
+const err = new Error(`Expected className to be a string but instead got ${typeof className}`);
+
 export default function spred(...classes) {
     let classList = [];
 
@@ -12,10 +14,20 @@ export default function spred(...classes) {
                     classList = [...classList, key];
                 }
             });
-        } else {
+        } else if (isString(className)) {
             classList = [...classList, className];
+        } else {
+            throw err;
         }
     });
 
     return classList.join(' ');
+}
+
+export function toggleClass(shouldShow, className) {
+    if (className && isString(className)) {
+        return shouldShow ? className : '';
+    }
+
+    throw err;
 }
