da9f7924604d3788692f1936f4652694adce9211
Marcel O. Coelho <flasd2@gmail.com>
2018-01-13 03:22:47 -0200

    Freshly backed files from webpack

--- dist/react-classlist-helper.js
+++ dist/react-classlist-helper.js
@@ -1,51 +1,84 @@
-'use strict';
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["reactClasslistHelper"] = factory();
+	else
+		root["reactClasslistHelper"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+"use strict";
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
-exports.isBoolean = isBoolean;
-exports.isObject = isObject;
-exports.isString = isString;
-exports.isUndefined = isUndefined;
-/**
- *
- * @param {*} value Any value
- * @return {boolean} assertion
- */
-function isBoolean(value) {
-  return typeof value === 'boolean' || (typeof value === 'undefined' ? 'undefined' : _typeof(value)) === 'object' && value !== null && typeof value.valueOf() === 'boolean';
-}
-
-/**
- *
- * @param {*} value Any value
- * @return {boolean} assertion
- */
-function isObject(value) {
-  return value !== null && (typeof value === 'undefined' ? 'undefined' : _typeof(value)) === 'object';
-}
-
-/**
- *
- * @param {*} value Any value
- * @return {boolean} assertion
- */
-function isString(value) {
-  return typeof value === 'string' || value instanceof String;
-}
-
-/**
- *
- * @param {*} value Any value
- * @return {boolean} assertion
- */
-function isUndefined(value) {
-  return typeof value === 'undefined';
-}
-'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
@@ -57,7 +90,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 exports.classList = classList;
 exports.toggleClass = toggleClass;
 
-var _assertions = require('./assertions');
+var _assertions = __webpack_require__(1);
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
@@ -146,3 +179,60 @@ var tC = exports.tC = toggleClass;
 var defaultFunc = classList;
 
 exports.default = defaultFunc;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+exports.isBoolean = isBoolean;
+exports.isObject = isObject;
+exports.isString = isString;
+exports.isUndefined = isUndefined;
+/**
+ *
+ * @param {*} value Any value
+ * @return {boolean} assertion
+ */
+function isBoolean(value) {
+  return typeof value === 'boolean' || (typeof value === 'undefined' ? 'undefined' : _typeof(value)) === 'object' && value !== null && typeof value.valueOf() === 'boolean';
+}
+
+/**
+ *
+ * @param {*} value Any value
+ * @return {boolean} assertion
+ */
+function isObject(value) {
+  return value !== null && (typeof value === 'undefined' ? 'undefined' : _typeof(value)) === 'object';
+}
+
+/**
+ *
+ * @param {*} value Any value
+ * @return {boolean} assertion
+ */
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
+}
+
+/**
+ *
+ * @param {*} value Any value
+ * @return {boolean} assertion
+ */
+function isUndefined(value) {
+  return typeof value === 'undefined';
+}
+
+/***/ })
+/******/ ]);
+});
